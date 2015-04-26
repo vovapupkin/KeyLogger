a678da74729f77da5b4d6a8dc203556e26a75af8
vovapupkin <bsuir@live.ru>
2015-04-26 21:20:25 +0300

    Add creating new file with path - current data

--- KeyboardLoggerVisualStudioProject/KeyboardLoggerVisualStudioProject/Source.cpp
+++ KeyboardLoggerVisualStudioProject/KeyboardLoggerVisualStudioProject/Source.cpp
@@ -1,9 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 
+HANDLE	file;
+
 bool CreateLogFile()
 {
+	CHAR path[MAX_PATH];
+	SYSTEMTIME st;
+	DWORD fileNum = 1;
+	GetLocalTime(&st);
+	GetDateFormat(LOCALE_SYSTEM_DEFAULT, 0, &st, "dd.MM.yy", path, MAX_PATH);
+	char * ptrAfterSyl_ = path + strlen(path) + 1;
+	strcat(path, "_1.txt");
 
+	file = CreateFile(path, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+	while (GetLastError() == ERROR_FILE_EXISTS)
+	{
+		fileNum++;
+		wsprintf(ptrAfterSyl_, "%d", fileNum);
+		strcat(path, ".txt");
+		file = CreateFile(path, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+	}
+	return file != INVALID_HANDLE_VALUE;
 }
 
 int main()
@@ -11,6 +29,11 @@ int main()
 	HANDLE mutex = CreateMutex(NULL, FALSE, "KeyboardLogger");
 	if (GetLastError() == ERROR_ALREADY_EXISTS || GetLastError() == ERROR_ACCESS_DENIED) 
 		return 1;
+	if (!CreateLogFile())
+	{
+		ReleaseMutex(mutex);
+		return 1;
+	}
 
 	return 0;
 }
